5f9053bcd9c94ac4f58c49d50ef7c10b960725f8
Aurélien <aurelien-barbier@outlook.com>
2017-04-28 10:07:58 +0200

    Fixtures pour HISTORY

---
+++ src/SubjectBundle/DataFixtures/ORM/LoadStoriesData.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SubjectBundle\DataFixtures\ORM;
+
+use CommitStripBundle\Entity\Card;
+use CommitStripBundle\Entity\Story;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadStoriesData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $stories = array(
+            array(
+                "titre" => 'Un de plus',
+                "cards" => array(
+                    array(
+                        "picture" => "http://wcs-fontainebleau.fr/preprod/checkpoint/chapter_1.png",
+                        "nbcard" => 1
+                        ),
+                    array(
+                        "picture" => "http://wcs-fontainebleau.fr/preprod/checkpoint/chapter_2.png",
+                        "nbcard" => 2
+                        ),
+                    array(
+                        "picture" => "http://wcs-fontainebleau.fr/preprod/checkpoint/chapter_3.png",
+                        "nbcard" => 3
+                        ),
+                    array(
+                        "picture" => "http://wcs-fontainebleau.fr/preprod/checkpoint/chapter_4.png",
+                        "nbcard" => 4
+                        ),
+                    ),
+                ),
+            );
+
+        foreach ($stories as $story){
+
+            $o_story = new Story();
+            $o_story->setTitre($story['titre']);
+            $manager->persist($o_story);
+
+            foreach ($story['cards'] as $card){
+                $o_card = new Card();
+                $o_card->setPicture($card['picture']);
+                $o_card->setNbcard($card['nbcard']);
+                $o_card->setStory($o_story);
+                $manager->persist($o_card);
+            }
+        }
+        $manager->flush();
+    }
+}
